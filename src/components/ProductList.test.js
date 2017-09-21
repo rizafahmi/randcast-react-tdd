5e76200b4b094db50e35b278f4751c532835ee18
Riza Fahmi <rizafahmi@gmail.com>
2017-09-21 08:50:06 +0700

    Third test on ProductList, check there is a product name

--- src/components/ProductList.test.js
+++ src/components/ProductList.test.js
@@ -16,3 +16,14 @@ test('ProductList should have list of products as ul and li', () => {
   const wrapper = shallow(<ProductList products={mockProducts} />)
   expect(wrapper.find('li').length).toEqual(mockProducts.length)
 })
+test('ProductList should have product name', () => {
+  const mockProducts = [
+    { id: 1, name: 'Mock product 1', brand: 'MockBrandA' },
+    { id: 2, name: 'Mock product 2', brand: 'MockBrandB' },
+    { id: 3, name: 'Mock product 3', brand: 'MockBrandC' }
+  ]
+
+  const wrapper = shallow(<ProductList products={mockProducts} />)
+  const firstElement = wrapper.find('li').first()
+  expect(firstElement.contains(mockProducts[0].name)).toEqual(true)
+})
