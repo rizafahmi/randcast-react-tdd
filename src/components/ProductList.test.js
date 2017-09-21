c74ad4a32b842dced2cec9f058f7d60e4dd1d5f3
Riza Fahmi <rizafahmi@gmail.com>
2017-09-21 08:11:54 +0700

    2nd test mock product list

--- src/components/ProductList.test.js
+++ src/components/ProductList.test.js
@@ -6,3 +6,13 @@ test('should render <ProductList />', () => {
   const wrapper = shallow(<ProductList />)
   expect(wrapper).toBeDefined()
 })
+test('ProductList should have list of products as ul and li', () => {
+  const mockProducts = [
+    { id: 1, name: 'Mock product 1', brand: 'MockBrandA' },
+    { id: 2, name: 'Mock product 2', brand: 'MockBrandB' },
+    { id: 3, name: 'Mock product 3', brand: 'MockBrandC' }
+  ]
+
+  const wrapper = shallow(<ProductList products={mockProducts} />)
+  expect(wrapper.find('li').length).toEqual(mockProducts.length)
+})
