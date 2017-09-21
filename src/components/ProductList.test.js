045890c2a042ab98f84d55d9465cde740ff8767f
Riza Fahmi <rizafahmi@gmail.com>
2017-09-21 08:54:05 +0700

    Refactor test using beforeEach

--- src/components/ProductList.test.js
+++ src/components/ProductList.test.js
@@ -2,28 +2,24 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import ProductList from './ProductList'
 
+let mockProducts, wrapper
+
+beforeEach(() => {
+  mockProducts = [
+    { id: 1, name: 'Mock product 1', brand: 'MockBrandA' },
+    { id: 2, name: 'Mock product 2', brand: 'MockBrandB' },
+    { id: 3, name: 'Mock product 3', brand: 'MockBrandC' }
+  ]
+
+  wrapper = shallow(<ProductList products={mockProducts} />)
+})
 test('should render <ProductList />', () => {
-  const wrapper = shallow(<ProductList products={[]} />)
   expect(wrapper).toBeDefined()
 })
 test('ProductList should have list of products as ul and li', () => {
-  const mockProducts = [
-    { id: 1, name: 'Mock product 1', brand: 'MockBrandA' },
-    { id: 2, name: 'Mock product 2', brand: 'MockBrandB' },
-    { id: 3, name: 'Mock product 3', brand: 'MockBrandC' }
-  ]
-
-  const wrapper = shallow(<ProductList products={mockProducts} />)
   expect(wrapper.find('li').length).toEqual(mockProducts.length)
 })
 test('ProductList should have product name', () => {
-  const mockProducts = [
-    { id: 1, name: 'Mock product 1', brand: 'MockBrandA' },
-    { id: 2, name: 'Mock product 2', brand: 'MockBrandB' },
-    { id: 3, name: 'Mock product 3', brand: 'MockBrandC' }
-  ]
-
-  const wrapper = shallow(<ProductList products={mockProducts} />)
   const firstElement = wrapper.find('li').first()
   expect(firstElement.contains(mockProducts[0].name)).toEqual(true)
 })
